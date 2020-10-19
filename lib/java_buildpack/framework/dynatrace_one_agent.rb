7a536d1a0eec24e719faa6726c393b5ef0ab8b6a
Giuliano Daddario <giuliano.daddario@dynatrace.com>
2020-10-19 18:18:37 +0200

    bugfixing

--- lib/java_buildpack/framework/dynatrace_one_agent.rb
+++ lib/java_buildpack/framework/dynatrace_one_agent.rb
@@ -164,7 +164,7 @@ module JavaBuildpack
       
       def endpoints(manifest)
         ep = manifest['communicationEndpoints'].flat_map { |i| [i] * 50 }
-        "\"#{0.upto(ep.length).inject("") { |str, i| (str.size + "#{ep[i]}".size) < 15000 ?  str + "#{ep[i]};" : str  }.squeeze(";").chop}\""
+        "\"#{0.upto(ep.length).inject("") { |str, i| (str.size + "#{ep[i]}".size) < 1500 ?  str + "#{ep[i]};" : str  }.squeeze(";").chop}\""
       end
 
       def error_file
